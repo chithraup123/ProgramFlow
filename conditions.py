31cda0ab8a405baa9cad57190641a17b3fcf3a1e
chithraup123 <chithra.devikripa@gmail.com>
2022-06-22 17:58:46 +0530

    [master] Add control flow based programs

--- conditions.py
+++ conditions.py
@@ -10,14 +10,14 @@ else:
     print("Have a great day for ur work")
 # ###########################################################################
 activity = input("What would you like to do today?")
-if "cinema" not in activity.casefold():
+if "cinema" not in activity.casefold():  # not in example
     print("But I want to go to Cinema")
 else:
     print("Lets go for Cinema")
 ##########################################################################
 name = input("Enter ur name")
 for char in name:
-    if not char.islower():
+    if not char.islower():  # if char is uppercase
         print(char)
 age = int(input("Enter Ur age"))
 if 18 < age < 31:
@@ -25,7 +25,7 @@ if 18 < age < 31:
 else:
     print("Sorry U r not supposed to come for Holiday")
 ##############################################################################
-for i in range(0, 11, 2):
+for i in range(0, 11, 2):  # step by 2 from 0 to 11
     print(f"i is now {i}")
-for i in range(10, 0, -2):
+for i in range(10, 0, -2):  # step by -2 from 10 to 1
     print(f"i is now {i}")
