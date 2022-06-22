d98aa7a51477ee189b4438d233c59cec1534d6df
chithraup123 <chithra.devikripa@gmail.com>
2022-06-22 17:54:42 +0530

    [master] Add control flow based programs

--- conditions.py
+++ conditions.py
@@ -1,29 +1,29 @@
-# age = int(input("What is ur Age? "))
-# if 18 <= age <= 65:
-#     print("Have a good day at work")
-# else:
-#     print("Enjoy ur free time")
-# print("~" * 70)
-# if age < 18 or age > 65:
-#     print("Enjoy the free time")
-# else:
-#     print("Have a great day for ur work")
+age = int(input("What is ur Age? "))
+if 18 <= age <= 65:
+    print("Have a good day at work")
+else:
+    print("Enjoy ur free time")
+print("~" * 70)
+if age < 18 or age > 65:
+    print("Enjoy the free time")
+else:
+    print("Have a great day for ur work")
 # ###########################################################################
-# activity = input("What would you like to do today?")
-# if "cinema" not in activity.casefold():
-#     print("But I want to go to Cinema")
-# else:
-#     print("Lets go for Cinema")
+activity = input("What would you like to do today?")
+if "cinema" not in activity.casefold():
+    print("But I want to go to Cinema")
+else:
+    print("Lets go for Cinema")
 ##########################################################################
-# name = input("Enter ur name")
-# for char in name:
-#     if not char.islower():
-#         print(char)
-# age = int(input("Enter Ur age"))
-# if 18 < age < 31:
-#     print("You are welcome to the Holiday")
-# else:
-#     print("Sorry U r not supposed to come for Holiday")
+name = input("Enter ur name")
+for char in name:
+    if not char.islower():
+        print(char)
+age = int(input("Enter Ur age"))
+if 18 < age < 31:
+    print("You are welcome to the Holiday")
+else:
+    print("Sorry U r not supposed to come for Holiday")
 ##############################################################################
 for i in range(0, 11, 2):
     print(f"i is now {i}")
